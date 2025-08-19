e87709d0bdfc10beebc75d5636f66140be35a1d9
Ducnguyn97 <nguyenduc8197@gmail.com>
2025-08-19 10:47:48 +0700

    commit file pratice2

---
+++ pratice2/src/Test.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Test {
+    public static void main(String[] args) {
+        Student student1 = new Student("Nam",20,"HN");
+        Student student2 = new Student("Hoang",21,"HY");
+        Student student3 = new Student("Quan",20,"NB");
+        Student student4 = new Student("Tien",20,"ND");
+
+        Map<Integer,Student> studentMap = new HashMap<>();
+        studentMap.put(1,student1);
+        studentMap.put(2,student2);
+        studentMap.put(3,student3);
+        studentMap.put(4,student4);
+
+        for(Map.Entry<Integer,Student> student : studentMap.entrySet()){
+            System.out.println(student.toString());
+        }
+        System.out.println(".............Set");
+        Set<Student> students = new HashSet<Student>();
+        students.add(student1);
+        students.add(student2);
+        students.add(student3);
+        students.add(student4);
+
+        for(Student student : students){
+            System.out.println(student.toString());
+        }
+    }
+}
